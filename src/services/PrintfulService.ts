f32044926186645c41d3105afd7bb70d3c8c11af
Liam Frager <liam.frager@gmail.com>
2025-02-06 17:40:41 -0500

    fix PrintfulService return statements

--- src/services/PrintfulService.ts
+++ src/services/PrintfulService.ts
@@ -7,13 +7,13 @@ export default class PrintfulService {
     // Returns all sync products from the Printful shop.
     public static async getAllProducts() {
         const response = await axios.get(`${this.apiEndpoint}/products`);
-        return response.data.result;
+        return response.data;
     }
 
     // Takes a Printful sync product ID as an input and returns details on the product
     public static async getProduct(id: number) {
         const response = await axios.get(`${this.apiEndpoint}/products/${id}`);
-        return response.data.result;
+        return response.data;
     }
 
     // Takes a Printful sync product ID as an input and returns details on all its variants
@@ -26,13 +26,13 @@ export default class PrintfulService {
     // Takes a Printful sync variant ID as an input and returns details on that variant
     public static async getVariant(id: number) {
         const response = await axios.get(`${this.apiEndpoint}/variants/${id}`);
-        return response.data.result.sync_variant;
+        return response.data.sync_variant;
     }
 
     // Takes a Printful product variant ID as an input and returns the color code associated with that variant
     public static async getColorCode(id: number) {
         const response = await axios.get(`${this.apiEndpoint}/products/variants/${id}`);
-        return response.data.result.variant.color_code;
+        return response.data.variant.color_code;
     }
 
     // Takes Printful order data as an input and places an order. Returns an API response from Printful
