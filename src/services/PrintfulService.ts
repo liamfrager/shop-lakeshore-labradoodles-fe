c1e4051873ffb68d939bf59ed4435e7f4935633b
Liam Frager <liam.frager@gmail.com>
2025-02-06 17:34:54 -0500

    rework PrintfulService to use backend

--- src/services/PrintfulService.ts
+++ src/services/PrintfulService.ts
@@ -1,32 +1,19 @@
 import axios from 'axios';
 import { Order } from '../types';
 
-const PRINTFUL_AUTH_TOKEN = process.env.REACT_APP_PRINTFUL_AUTH_TOKEN;
-const AUTO_FULFILL_PRINTFUL_ORDERS = process.env.REACT_APP_AUTO_FULFILL_PRINTFUL_ORDERS === 'True';
-
 export default class PrintfulService {
-    private static authToken = PRINTFUL_AUTH_TOKEN;
-    private static apiEndpoint = 'https://api.printful.com';
-    private static apiHeaders = {
-        'Authorization': 'Bearer ' + this.authToken
-    };
+    private static apiEndpoint = process.env.REACT_APP_BE_ENDPOINT;
 
     // Returns all sync products from the Printful shop.
     public static async getAllProducts() {
-        const response = await axios.get(`${this.apiEndpoint}/sync/products`, {
-            headers: this.apiHeaders,
-            params: {status: 'all'},
-        });
+        const response = await axios.get(`${this.apiEndpoint}/products`);
         return response.data.result;
     }
 
     // Takes a Printful sync product ID as an input and returns details on the product
     public static async getProduct(id: number) {
-        const response = await axios.get(`${this.apiEndpoint}/sync/products/${id}`, {
-            headers: this.apiHeaders,
-            params: {limit: 100},
-        });
-        return response.data['result'];
+        const response = await axios.get(`${this.apiEndpoint}/products/${id}`);
+        return response.data.result;
     }
 
     // Takes a Printful sync product ID as an input and returns details on all its variants
@@ -38,26 +25,20 @@ export default class PrintfulService {
 
     // Takes a Printful sync variant ID as an input and returns details on that variant
     public static async getVariant(id: number) {
-        const response = await axios.get(`${this.apiEndpoint}/sync/variant/${id}`, {
-            headers: this.apiHeaders,
-        });
+        const response = await axios.get(`${this.apiEndpoint}/variants/${id}`);
         return response.data.result.sync_variant;
     }
 
     // Takes a Printful product variant ID as an input and returns the color code associated with that variant
     public static async getColorCode(id: number) {
-        const response = await axios.get(`${this.apiEndpoint}/products/variant/${id}`, {
-            headers: this.apiHeaders,
-        });
+        const response = await axios.get(`${this.apiEndpoint}/products/variants/${id}`);
         return response.data.result.variant.color_code;
     }
 
     // Takes Printful order data as an input and places an order. Returns an API response from Printful
     public static async placeOrder(order: Order) {
         const response = await axios.post(`${this.apiEndpoint}/orders`, {
-            headers: this.apiHeaders,
             body: order,
-            params: {'confirm': AUTO_FULFILL_PRINTFUL_ORDERS},
         });
         return response.data;
     }
