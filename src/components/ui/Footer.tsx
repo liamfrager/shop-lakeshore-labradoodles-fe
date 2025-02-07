db9a95cbc926d5f945cb1c978a39422c12cd1367
Liam Frager <liam.frager@gmail.com>
2025-02-07 14:48:39 -0500

    add footer styles

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,7 +1,13 @@
+import './Footer.css';
+
 export default function Footer() {
     return (
         <footer>
-            <span>{`Copyright © ${new Date().getFullYear()}`}</span><a href="https://lakeshorelabradoodles.com">Lakeshore Labradoodles</a><span>. All rights reserved.</span>
-        </footer>
+            <span>{`Copyright © ${new Date().getFullYear()} `}</span>
+            <a href="https://lakeshorelabradoodles.com" target="_blank" rel="noopener noreferrer">
+                Lakeshore Labradoodles
+            </a>
+            <span>. All rights reserved.</span>
+        </footer >
     )
 }
