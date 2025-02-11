ea5c2cbddf20e73ea0d08eb29fe2353ac3c90b90
Liam Frager <liam.frager@gmail.com>
2025-02-10 23:10:19 -0500

    hide price increase in size select

--- src/components/product-display/ProductSizeSelection.tsx
+++ src/components/product-display/ProductSizeSelection.tsx
@@ -1,3 +1,5 @@
+import { SetStateAction, useEffect, useState } from "react";
+
 interface ProductSizeSelectionProps {
     sizes: string[],
     sizePrices: { [size: string]: number },
@@ -5,14 +7,36 @@ interface ProductSizeSelectionProps {
 }
 
 export default function ProductSizeSelection(props: ProductSizeSelectionProps) {
+    const [selectedSize, setSelectedSize] = useState<string>(props.sizes[0]);
+
+    const handleOnChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const select = e.target;
+        const newSize = select.value;
+        const selected = document.createElement('option');
+        selected.value = newSize;
+        selected.textContent = newSize;
+        select.appendChild(selected)
+        select.selectedIndex = select.options.length - 1;
+        setSelectedSize(newSize);
+        props.onChange(newSize);
+    }
+
+    const handleOnClick = (e: React.MouseEvent<HTMLSelectElement>) => {
+        const select = e.currentTarget;
+        if (select.options.length > props.sizes.length) {
+            select.removeChild(select.lastChild!);
+            select.selectedIndex = props.sizes.indexOf(selectedSize);
+        }
+    }
+
     return (
-        <select onChange={(e) => props.onChange(e.target.value)}>
+        <select onChange={handleOnChange} onMouseDown={handleOnClick}>
             {props.sizes.map(size => {
                 const priceIncrease: number = props.sizePrices[size] - props.sizePrices[props.sizes[0]];
+                const displayText = priceIncrease > 0 ? `${size} (+$${priceIncrease.toFixed(2)})` : size;
                 return (
                     <option key={size} value={size}>
-                        {size}
-                        {priceIncrease > 0 && ` (+$${priceIncrease.toFixed(2)})`}
+                        {displayText}
                     </option>
                 )
             })}
